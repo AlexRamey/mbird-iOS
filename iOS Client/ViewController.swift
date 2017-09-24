8d0a4c32ddf5abd4a309b357c3f3f57e5e09979d
AlexRamey <alex.ramey@willowtreeapps.com>
2017-09-24 18:48:48 -0400

    Initial Commit

---
+++ iOS Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Client
+//
+//  Created by Alex Ramey on 9/24/17.
+//  Copyright © 2017 Mockingbird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
