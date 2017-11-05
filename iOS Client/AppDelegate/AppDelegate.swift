dc4d7a61a309a9878f13a878dddf6d84b48e0bbb
AlexRamey <alex.ramey@willowtreeapps.com>
2017-11-05 08:33:14 -0500

    set minimum fetch interval for background fetch

--- iOS Client/AppDelegate/AppDelegate.swift
+++ iOS Client/AppDelegate/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.appCoordinator = AppCoordinator(window: self.window!)
         self.appCoordinator.start()
+        application.setMinimumBackgroundFetchInterval(MBConstants.SECONDS_IN_A_DAY)
         return true
     }
 
