04e2acd8b491ea363d971b7ee8565a6d7eac91ea
Alex Ramey <abr8xq@virginia.edu>
2017-11-13 22:08:47 -0500

    1. Render links in SafariViewController 2. Updated CSS colors to matc… (#15)
    * 1. Render links in SafariViewController 2. Updated CSS colors to match mbird websit more closely
    
    * 1. Ignore embed.vevo.com links 2. Up article count to 50
    
    * 1. Disable link-following when we handle it manually (this prevents the user from getting the safari overlay and also redirected to the youtube app)

--- iOS Client/ArticlesController/ArticlesCoordinator.swift
+++ iOS Client/ArticlesController/ArticlesCoordinator.swift
@@ -9,11 +9,13 @@
 import Foundation
 import UIKit
 import ReSwift
+import SafariServices
 
-class ArticlesCoordinator: NSObject, Coordinator, StoreSubscriber, UINavigationControllerDelegate {
+class ArticlesCoordinator: NSObject, Coordinator, StoreSubscriber, UINavigationControllerDelegate, SFSafariViewControllerDelegate {
     var childCoordinators: [Coordinator] = []
     var route: [RouteComponent] = [.base]
     var tab: Tab = .articles
+    var overlay: URL? = nil
     
     var rootViewController: UIViewController {
         return self.navigationController
@@ -32,10 +34,28 @@ class ArticlesCoordinator: NSObject, Coordinator, StoreSubscriber, UINavigationC
     
     // MARK: - StoreSubscriber
     func newState(state: MBAppState) {
-        guard state.navigationState.selectedTab == .articles, let newRoute = state.navigationState.routes[.articles] else {
+        guard state.navigationState.selectedTab == .articles,
+            let newRoute = state.navigationState.routes[.articles] else {
             return
         }
         build(newRoute: newRoute)
         route = newRoute
+        
+        let articlesSafariOverlay: URL? = state.navigationState.safariOverlays[.articles]!
+        if self.overlay == nil, let overlay = articlesSafariOverlay {
+            self.overlay = overlay
+            let overlayVC = SFSafariViewController(url: overlay)
+            overlayVC.delegate = self
+            self.navigationController.present(overlayVC, animated: true, completion: nil)
+        } else if self.overlay != nil, articlesSafariOverlay == nil {
+            self.overlay = nil
+            self.navigationController.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - SafariViewControllerDelegate
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        MBStore.sharedStore.dispatch(SelectedArticleLink(url: nil))
+        self.overlay = nil
     }
 }
