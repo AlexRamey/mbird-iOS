90aa2eae3959cbfe98e56f5ac1487946f34657f2
Jonathan Witten <jonathanwitten@willowtreeapps.com>
2017-12-10 16:48:49 -0500

    sets up core data models for podcasts

--- iOS Client/Store/XMLParsingDelegate.swift
+++ iOS Client/Store/XMLParsingDelegate.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+
+class PodcastXMLParsingDelegate: NSObject, XMLParserDelegate {
+    var podcast: [String: String] = [:]
+    var items: [[String:String]] = []
+    var foundCharacters: String = ""
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        self.foundCharacters += string
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "title" {
+            self.podcast["title"] = self.foundCharacters.trimmingCharacters(in: .whitespacesAndNewlines)
+        } else if elementName == "author" {
+            self.podcast["itunes:author"] = self.foundCharacters.trimmingCharacters(in: .whitespacesAndNewlines)
+        } else if elementName == "description" {
+            self.podcast["description"] = self.foundCharacters.trimmingCharacters(in: .whitespacesAndNewlines)
+        } else if elementName == "itunes:image" {
+            self.podcast["image"] = self.foundCharacters.trimmingCharacters(in: .whitespacesAndNewlines)
+        } else if elementName == "guid" {
+            self.podcast["guid"] = self.foundCharacters.trimmingCharacters(in: .whitespacesAndNewlines)
+        } else if elementName == "pubDate" {
+            self.podcast["pubDate"] = self.foundCharacters.trimmingCharacters(in: .whitespacesAndNewlines)
+        } else if elementName == "itunes:keywords" {
+            self.podcast["keywords"] = self.foundCharacters.trimmingCharacters(in: .whitespacesAndNewlines)
+        } else if elementName == "itunes:duration" {
+            self.podcast["duration"] = self.foundCharacters.trimmingCharacters(in: .whitespacesAndNewlines)
+        } else if elementName == "item" {
+            let tempPod = self.podcast
+            self.items.append(tempPod)
+            self.podcast = [:]
+        }
+        self.foundCharacters = ""
+    }
+}
