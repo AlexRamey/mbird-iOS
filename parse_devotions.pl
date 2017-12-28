2cd3c2604638128722d33b89fcaad67b1549ed2d
AlexRamey <alex.ramey@willowtreeapps.com>
2017-12-27 23:03:02 -0500

    Finished parse devotions script

--- parse_devotions.pl
+++ parse_devotions.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
-
+# Note: Be sure to replace smart, curly quotes with straight double quotes in the input file
+# before passing it through this script
 use strict;
 use warnings;
 
@@ -26,31 +27,62 @@ $devotionsText .= $_;
 ## VERSE_TEXT (VERSE)
 ## TEXT . . .
 ## TEXT CONTINUED . . .
-## DATE
+## NEXT DATE
+### . . .
+
+## Build array of all date strings
 my $month;
 for $month (@months) {
     my $dayCounter = 1;
-    while ($dayCounter < $days_per_month{$month} + 1) {
+    while ($dayCounter <= $days_per_month{$month}) {
         my $currentDate = "$month $dayCounter";
         push @dates, $currentDate;
         $dayCounter += 1;
     }
 }
 
-# could split on dates . . . 
-# todo: match first line after each date
-# todo: match verse at end of first line of each date
-# todo: match everything between first line and next date or end of string
+## Split the text on dates
+my $regEx = join(" \\d+\\s|", @months) . " \\d+\\s";
+my @devotions = split(/$regEx/, $devotionsText);
+shift @devotions; # Knock off whatever preceded the first date (probably empty string)
 
-foreach my $i (0 .. $#dates) {
-    my $firstDate = $dates[$i];
-    my $secondDate = $i == $#dates ? "" : $dates[$i+1];
-    my $regEx = "$firstDate";
-    print($regEx);
-    if ($devotionsText =~ m/$regEx/) {
-        print($1);
-        die "WIN!";
+if ($#devotions != 365) {
+    die "Did not find 366 devotions! Did someone forget about February 29th!!!";
+}
+
+## For each devotion, grab the verseText, verse, and text
+## Strategy, just match the verse itself, which is in parentheses at end of line 1
+## Then leverage the prematch and postmatch variables to get the verseText and devotion text.
+my $devotion;
+for $devotion (@devotions) {
+    my $verseRegEx = '\((.+)\)\s';
+    if ($devotion =~ m/$verseRegEx/) {
+        push @verseTexts, $`;   # $` is the special prematch variable
+        push @verses, $1;       # $1 refers to the text matched by the (.+) group (back-ref)
+        push @texts, $';        # $` is the special postmatch variable
     } else {
-        die "ERROR!";
+        die "verse regex failed to match";
     }
 }
+
+## Step 3: Output JSON
+print "[";
+foreach my $i (0 .. $#dates) {
+    # Sanitize outputs by replacing double quotes with single quotes
+    # and trimming trailing/leading whitespace
+    $dates[$i] =~ s/"/'/g;
+    $dates[$i] =~ s/^\s+|\s+$//g;
+    $verses[$i] =~ s/"/'/g;
+    $verses[$i] =~ s/^\s+|\s+$//g;
+    $verseTexts[$i] =~ s/"/'/g;
+    $verseTexts[$i] =~ s/^\s+|\s+$//g;
+    $texts[$i] =~ s/"/'/g;
+    $texts[$i] =~ s/^\s+|\s+$//g;
+    print "\n\t{\n";
+    print "\t\t\"date\": \"$dates[$i]\",\n";
+    print "\t\t\"verse\": \"$verses[$i]\",\n";
+    print "\t\t\"verseText\": \"$verseTexts[$i]\",\n";
+    print "\t\t\"text\": \"$texts[$i]\"\n";
+    print ("\t}" . (($i eq "$#dates") ? "" : ","));
+}
+print "\n]\n";
