b17f4d4224c8b796e793a583996b9b1824329989
AlexRamey <alex.ramey@willowtreeapps.com>
2018-01-14 13:11:00 -0500

    1. Made articles text size responsive to accessibility text size settings

--- iOS Client/ArticlesController/MBArticleDetailViewController.swift
+++ iOS Client/ArticlesController/MBArticleDetailViewController.swift
@@ -25,24 +25,55 @@ class MBArticleDetailViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureWebView()
-        configureBackButton()
-    }
-    
-    func configureWebView() {
+        
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.navigationDelegate = self
         view = webView
         
+        configureWebView()
+        
+        configureBackButton()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(contentSizeDidChange(_:)),
+                                               name: NSNotification.Name.UIContentSizeCategoryDidChange,
+                                               object: nil)
+    }
+    
+    @objc private func contentSizeDidChange(_ notification: Notification) {
+        configureWebView()
+    }
+    
+    func configureWebView() {
         if let content = self.selectedArticle?.content {
             print(content)
-            let cssHead: String = "<head>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"MB.css\">\n</head>\n"
+            // <head>
+            let contentTypeHead = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">"
+            let viewPortHead = "<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">"
+            let cssHead = "<link rel=\"stylesheet\" type=\"text/css\" href=\"MB.css\" />"
+            let head = "<head>\n\(contentTypeHead)\n\(viewPortHead)\n\(cssHead)\n</head>"
             
+            // <style>
+            let titleFontSize = UIFont.preferredFont(forTextStyle: .headline).pointSize
+            let authorFontSize = UIFont.preferredFont(forTextStyle: .subheadline).pointSize
+            let bodyFontSize = UIFont.preferredFont(forTextStyle: .body).pointSize
+            let style = ""
+                + "<style>\n"
+                + "p.title{font-size: \(titleFontSize)pt;}\n"
+                + "p.author{font-size: \(authorFontSize)pt;}\n"
+                + "body{font-size: \(bodyFontSize)pt;}\n"
+                + "</style>"
+            
+            
+            // <body>
             let title: String = self.selectedArticle?.title ?? "Untitled 👀"
             let author: String = self.selectedArticle?.author?.name ?? "Mockingbird Staff"
             let articleHeader = "<p class=\"title\">\(title)</p><p class=\"author\">By \(author)</p>"
-            let fullContent = cssHead + "<body>" + articleHeader + content + "</body>"
+            let body = "<body>" + articleHeader + content + "</body>"
+            
+            // full page
+            let fullContent = head + style + body
             
             // baseURL is used by the wkWebView to resolve relative links
             // Here, we point it straight to our css file referred to in the css header
