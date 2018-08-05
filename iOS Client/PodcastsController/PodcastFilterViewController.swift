da21854daf749dc2a27f1d4a920aaf5cca1aed32
Jonathan Witten <jonathan.witten@willowtreeapps.com>
2018-08-05 13:54:20 -0400

    Add support for option to just show downloaded podcasts (#102)
    * Add support for option to just show downloaded podcasts
    
    * clean up from PR comments

--- iOS Client/PodcastsController/PodcastFilterViewController.swift
+++ iOS Client/PodcastsController/PodcastFilterViewController.swift
@@ -14,11 +14,21 @@ protocol PodcastFilterHandler {
 
 class PodcastsFilterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, PodcastFilterDelegate {
     @IBOutlet weak var tableView: UITableView!
-    var streams: [PodcastStream] = []
+    var filterOptions: [PodcastFilterOption] = []
+    var enabledOptions: [PodcastFilterOption] = []
     var repository: PodcastsRepository!
     var handler: PodcastFilterHandler?
     
     let filterReuseIdentifier: String = "PodcastFilterTableViewCell"
+    
+    var shouldHideStreamOptions: Bool {
+        return enabledOptions.contains(where: {
+            switch $0 {
+            case .downloaded: return true
+            case .stream: return false
+            }
+        })
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +46,9 @@ class PodcastsFilterViewController: UIViewController, UITableViewDataSource, UIT
     }
     
     func loadData() {
-        self.streams = self.repository.getStreams()
+        self.filterOptions = self.repository.getStreams().map { PodcastFilterOption.stream($0) }
+        self.filterOptions.append(.downloaded)
+        self.enabledOptions = self.repository.getEnabledFilterOptions()
         self.tableView.reloadData()
     }
     
@@ -54,7 +66,11 @@ class PodcastsFilterViewController: UIViewController, UITableViewDataSource, UIT
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.streams.count
+        if shouldHideStreamOptions {
+            return 1
+        } else {
+            return self.filterOptions.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -62,13 +78,54 @@ class PodcastsFilterViewController: UIViewController, UITableViewDataSource, UIT
                 return UITableViewCell()
         }
         cell.delegate = self
-        let stream = streams[indexPath.row]
-        cell.configure(image: UIImage(named: stream.imageName), stream: stream, on: self.repository.getVisibleStreams().contains(stream))
+        let option: PodcastFilterOption
+        if shouldHideStreamOptions {
+            option = .downloaded
+        } else {
+            option = filterOptions[indexPath.row]
+        }
+        switch option {
+        case .stream(let stream):
+            cell.configure(image: UIImage(named: stream.imageName),
+                           title: stream.title,
+                           option: option,
+                           isOn: optionsContainsStream(enabledOptions, stream: stream))
+        case .downloaded:
+            cell.configure(image: UIImage(named: "download-done"),
+                           title: "Downloaded Podcasts",
+                           option: option,
+                           isOn: shouldHideStreamOptions)
+        }
         return cell
     }
     
     // MARK: - PodcastFilterDelegate
-    func filterStream(_ stream: PodcastStream, on: Bool) {
-        self.repository.setStreamVisible(stream: stream, isVisible: on)
+    func toggleFilterOption(_ option: PodcastFilterOption, isOn: Bool) {
+        self.repository.setFilter(option: option, isVisible: isOn)
+        loadData()
+    }
+    
+    private func optionsContainsStream(_ options: [PodcastFilterOption], stream: PodcastStream) -> Bool {
+        return options.contains(where: {
+            if case .stream(let optionStream) = $0 {
+                return optionStream == stream
+            } else {
+                return false
+            }
+        })
+    }
+}
+
+enum PodcastFilterOption {
+    case stream(PodcastStream)
+    case downloaded
+    
+    var key: String {
+        switch self {
+        case .stream(let stream):
+            return stream.rawValue
+        case .downloaded:
+            return "downloaded"
+        }
     }
 }
